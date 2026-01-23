0e04131e28ae7748fe1fdcb4093c2ab3be708d65
jayamehra077-ui <jayamehra077@gmail.com>
2026-01-23 11:24:40 +0530

    HTML

--- INDEX.js
+++ INDEX.js
@@ -1,10 +1,21 @@
 let btn=document.querySelector("button")
 let body=document.querySelector("body")
+let isdark=true
 btn.addEventListener("click",function()
 {
 // body.style.backgroundColor='pink'
 // body.style.backgroundColor='plum'
 // body.style.backgroundColor='aqua'
-body.style.backgroundColor=' green'
+// body.style.backgroundColor=' green'
+if(isdark)
+{
+    body.style.backgroundColor='plum'
+}
+else
+{
+    body.style.backgroundColor='aqua'
+
+}
+isdark=!isdark
 }
 )
